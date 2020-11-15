798bdbab3d8c6d8557260d7202fcf8df08ad5775
IchiroOsalde <ichiroosalde@gmail.com>
2020-11-15 15:42:53 -0600

    Conexión entre el menú de configuración y la pantalla inicial

--- app/src/main/java/com/example/quizapp2/MainActivity.kt
+++ app/src/main/java/com/example/quizapp2/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.quizapp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 private const val OPTIONS_ACTIVITY_REQUEST_CODE = 0;
@@ -11,7 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var  playButton : Button
     private lateinit var setButton : Button
-    private var categories : String = "Hola"
+
+    private lateinit var categories : String
     private var difficulty : Int = 0
     private var nquestions : Int = 0
     private var ghints : Boolean = false
@@ -24,20 +27,31 @@ class MainActivity : AppCompatActivity() {
         setButton = findViewById(R.id.settings_button)
 
         playButton.setOnClickListener { _ ->
+            Toast.makeText(this, difficulty.toString(),Toast.LENGTH_SHORT).show()
         }
 
         setButton.setOnClickListener { _ ->
             startActivityForResult(
-                Options.createIntent(
-                    this,
-                    categories,
-                    difficulty,
-                    nquestions,
-                    ghints
-                ),
+                Options.createIntent(this),
                 OPTIONS_ACTIVITY_REQUEST_CODE
             )
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            OPTIONS_ACTIVITY_REQUEST_CODE ->
+                when(resultCode){
+                    Options.RESULT_SETTINGS_CONFIG -> {
+                        categories = data!!.getStringExtra(Options.EXTRA_CATEGORIES_TEXT).toString()
+                        difficulty = data.getIntExtra(Options.EXTRA_DIFFICULTY_LEVEL,0)
+                        nquestions = data.getIntExtra(Options.EXTRA_QUESTION_NUMBERS,5)
+                        ghints = data.getBooleanExtra(Options.EXTRA_HINT_OPTION,false)
+                    }
+                    else-> null
+                }
+            else->super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
--- app/src/main/java/com/example/quizapp2/Options.kt
+++ app/src/main/java/com/example/quizapp2/Options.kt
@@ -4,26 +4,40 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Options : AppCompatActivity() {
     companion object{
+        const val RESULT_SETTINGS_CONFIG = RESULT_FIRST_USER
+
         const val EXTRA_CATEGORIES_TEXT = "com.example.quizapp2.categories_text"
         const val EXTRA_DIFFICULTY_LEVEL = "com.example.quizapp2.difficulty_level"
-        const val EXTRA_QUESTION_NUMBERS = "com.example.quizapp2.question_numbers"
+        const val EXTRA_QUESTION_NUMBERS = "com.example.quizapp2.question_number"
         const val EXTRA_HINT_OPTION = "com.example.quizapp2.hint_option"
 
-        fun createIntent(packageContext: Context, categories: String, difficulty: Int, nQuestions: Int, hints: Boolean):Intent{
+        fun createIntent(packageContext: Context):Intent{
             return Intent(packageContext, Options::class.java).apply{
-                putExtra(EXTRA_CATEGORIES_TEXT,categories)
-                putExtra(EXTRA_DIFFICULTY_LEVEL,difficulty)
-                putExtra(EXTRA_QUESTION_NUMBERS,nQuestions)
-                putExtra(EXTRA_HINT_OPTION,hints)
             }
         }
     }
 
+    private lateinit var saveButton : Button
+    private lateinit var cancelButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+
+        saveButton = findViewById(R.id.save_button)
+        cancelButton = findViewById(R.id.cancel_button)
+
+        saveButton.setOnClickListener { _ ->
+            setResult(RESULT_SETTINGS_CONFIG,Intent().apply {
+                putExtra(EXTRA_CATEGORIES_TEXT, "Hola")
+                putExtra(EXTRA_DIFFICULTY_LEVEL, 1)
+                putExtra(EXTRA_QUESTION_NUMBERS,6)
+                putExtra(EXTRA_HINT_OPTION,true)
+            })
+        }
     }
 }
