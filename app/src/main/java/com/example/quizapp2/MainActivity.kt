cee7c718fca67e72f07f00714c0a27909d3c2d82
IchiroOsalde <ichiroosalde@gmail.com>
2020-11-15 14:10:45 -0600

    funcionamiento del botón de opciones

--- app/src/main/java/com/example/quizapp2/MainActivity.kt
+++ app/src/main/java/com/example/quizapp2/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.quizapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+private const val OPTIONS_ACTIVITY_REQUEST_CODE = 0;
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  playButton : Button
+    private lateinit var setButton : Button
+    lateinit var categories : String
+    private var difficulty : Int = 0
+    private var nquestions : Int = 0
+    private var ghints : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton = findViewById(R.id.play_button)
+        setButton = findViewById(R.id.settings_button)
+
+        playButton.setOnClickListener { _ ->
+
+        }
+
+        setButton.setOnClickListener { _ ->
+            startActivityForResult(
+                Options.createIntent(
+                    this,
+                    categories,
+                    difficulty,
+                    nquestions,
+                    ghints
+                ),
+                OPTIONS_ACTIVITY_REQUEST_CODE
+            )
+        }
+
     }
 }
--- app/src/main/java/com/example/quizapp2/Options.kt
+++ app/src/main/java/com/example/quizapp2/Options.kt
@@ -1,9 +1,27 @@
 package com.example.quizapp2
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Options : AppCompatActivity() {
+    companion object{
+        const val EXTRA_CATEGORIES_TEXT = "com.example.quizapp2.categories_text"
+        const val EXTRA_DIFFICULTY_LEVEL = "com.example.quizapp2.difficulty_level"
+        const val EXTRA_QUESTION_NUMBERS = "com.example.quizapp2.question_numbers"
+        const val EXTRA_HINT_OPTION = "com.example.quizapp2.hint_option"
+
+        fun createIntent(packageContext: Context, categories: String, difficulty: Int, nQuestions: Int, hints: Boolean):Intent{
+            return Intent(packageContext, Options::class.java).apply{
+                putExtra(EXTRA_CATEGORIES_TEXT,categories)
+                putExtra(EXTRA_DIFFICULTY_LEVEL,difficulty)
+                putExtra(EXTRA_QUESTION_NUMBERS,nQuestions)
+                putExtra(EXTRA_HINT_OPTION,hints)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
