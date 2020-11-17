7ee00f9971d6779e9970988c45c644d33058a2e2
IchiroOsalde <ichiroosalde@gmail.com>
2020-11-16 21:01:50 -0600

    Cambio de API

--- app/src/main/java/com/example/quizapp2/MainActivity.kt
+++ app/src/main/java/com/example/quizapp2/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private var nquestions : Int = 5
     private var ghints : Int = 0
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
