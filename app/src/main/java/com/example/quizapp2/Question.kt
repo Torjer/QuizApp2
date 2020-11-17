4d991eedc9a0185f4d5a21152735dce6832922a8
Torjer <andres22jld@gmail.com>
2020-11-16 19:38:27 -0700

    Orden aleatorio de las respuestas

--- app/src/main/java/com/example/quizapp2/Question.kt
+++ app/src/main/java/com/example/quizapp2/Question.kt
@@ -1,3 +1,3 @@
 package com.example.quizapp2
 
-data class Question(var category: String, var resID: Int, var answer: Int, var qcolor : Int)
+data class Question(var category: String, var resID: Int, var answer: Int, var qcolor : Int, var ianswera : Int, var ianswerb : Int, var ianswerc : Int)
